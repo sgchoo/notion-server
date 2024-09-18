b92872dca5f3275ebd0f1306d87408740e648624
choosg <choosg@naver.com>
2024-09-18 18:27:20 +0900

    feat: 스케쥴 데이터 추가 자동화

--- services/notion.js
+++ services/notion.js
@@ -3,30 +3,23 @@ const { Client } = require('@notionhq/client')
 const schedule = require('node-schedule');
 
 const notion = new Client({
-    auth: process.env.NOTION_SECRET_TOKEN
+    auth: process.env.NOTION_SECRET_TOKEN || 'secret_vdTjF7R7pC39HsQ3Ee08HBk3FwrTrwVGpqfJGwdTi8T'
 })
-const database_todo_id = process.env.NOTION_TODO_DATABASE;
-const database_schedule_id = process.env.NOTION_SCHEDULES_DATABASE;
+const database_todo_id = process.env.NOTION_TODO_DATABASE || 'f4b6421eb2744cd0a2e45c8647e3603a';
+const database_schedule_id = 'f819516e02d64362ba2d0ac1637b5e03';
 
 module.exports.getCalendarList = async function getCalendarList() {
     const {results} = await notion.databases.query({
         database_id: database_schedule_id
     })
     const calendar = results.map((page) => {
-      console.log(page)
-      // console.log(page.날짜);
-      // console.log(page.태그.multi_select);
-      // console.log(page.이름.title);
       return {
         id: page.id,
         title: page.properties.이름.title[0].plain_text,
         date: page.properties.날짜.date,
-        tags: page.properties.태그,
-      //   tags: page.properties.Tags.rich_text[0].text.content,
-      //   description: page.properties.Description.rich_text[0].text.content,
+        tags: page.properties.태그.multi_select.map(tag => ({ name: tag.name })),
       };
     });
-    console.log(calendar);
   return calendar;
 }
 
@@ -54,24 +47,9 @@ module.exports.createPage = async function createPage(
             end: endDate,
           },
         },
-        // 태그: {
-        //   rich_text: [
-        //     {
-        //       text: {
-        //         content: tags,
-        //       },
-        //     },
-        //   ],
-        // },
-        // Description: {
-        //   rich_text: [
-        //     {
-        //       text: {
-        //         content: description,
-        //       },
-        //     },
-        //   ],
-        // },
+        태그: {
+          multi_select: tags,
+        },
       },
     });
     return response;
@@ -81,5 +59,9 @@ const job = schedule.scheduleJob('* * * * *', async () => {
   console.log('매 분 작업 실행 중...');
   const data = await this.getCalendarList();
 
-  console.log(data);
+  // console.log(data);
+
+  for (const item of data) {
+    await this.createPage(item.title, item.date.start, item.date.end, item.tags);
+  };
 })
